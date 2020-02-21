1b24e941401d83ddfb97fdbab3268445c4e5d959
Derek Etman <dereketman@gmail.com>
2020-02-20 20:41:33 -0800

    + added plan for project, + filled out BFS outline

--- README.md
+++ README.md
@@ -37,6 +37,10 @@ There are a few smaller graphs in the file which you can test your traversal met
 
 Start by writing an algorithm that picks a random unexplored direction from the player's current room, travels and logs that direction, then loops. This should cause your player to walk a depth-first traversal. When you reach a dead-end (i.e. a room with no unexplored paths), walk back to the nearest room that does contain an unexplored path.
 
+- Pick a room
+- Travel to that room
+- log that direction 
+
 You can find the path to the shortest unexplored room by using a breadth-first search for a room with a `'?'` for an exit. If you use the `bfs` code from the homework, you will need to make a few modifications.
 
 1. Instead of searching for a target vertex, you are searching for an exit with a `'?'` as the value. If an exit has been explored, you can put it in your BFS queue like normal.
@@ -44,6 +48,10 @@ You can find the path to the shortest unexplored room by using a breadth-first s
 2. BFS will return the path as a list of room IDs. You will need to convert this to a list of n/s/e/w directions before you can add it to your traversal path.
 
 If all paths have been explored, you're done!
+
+
+Derek Planning, We're going to search through rooms using BFS for rooms  that have a false value, once it has been entered it will change to true.
+
 
 ## Minimum Viable Product
 
--- adv.py
+++ adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Queue
 
 import random
 from ast import literal_eval
@@ -11,10 +12,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,6 +30,20 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# You may find the commands `player.current_room.id`, `player.current_room.get_exits()` and `player.travel(direction)` useful.``
+
+def bfs(self, ):
+    # Create an empty Queue
+    # Add a path to the room origin to the queue
+    # create an empty set to store the visited rooms
+    # while the queue is not empty
+        # Dequeue the first path
+        # grab the last room from the path
+        # check if it's been visited....
+        # if it hasnt been visited
+            # mark it as visited
+            # then add a path to all neighbors to the back of the queue
+                # make a copy of the path before adding.
 
 
 # TRAVERSAL TEST
@@ -51,12 +66,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
